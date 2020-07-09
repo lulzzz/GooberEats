b8916f3780a363ad3a09f89d92fdf8a2471dead6
Robert James Nielsen <robert.j.nielsen@outlook.com>
2020-07-09 14:42:48 -0700

    Added IPlaces interface and PlacesService service.

---
+++ GooberEats/GooberEatsAPI/Models/Interfaces/IPlaces.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GooberEatsAPI.Models.Interfaces
+{
+    public interface IPlaces
+    {
+        public Task<List<Place>> GetPlaces();
+    }
+}
---
+++ GooberEats/GooberEatsAPI/Models/Services/PlacesService.cs
@@ -0,0 +1,16 @@
+﻿using GooberEatsAPI.Models.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GooberEatsAPI.Models.Services
+{
+    public class PlacesService : IPlaces
+    {
+        public Task<List<Place>> GetPlaces()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
