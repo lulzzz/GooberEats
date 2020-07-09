15979d39bad352df2a1461ac3084be9d6a57721a
Robert James Nielsen <robert.j.nielsen@outlook.com>
2020-07-09 12:16:18 -0700

    MVC / Controller proof of concept success.

---
+++ GooberEats/GooberEatsAPI/Controllers/PlacesController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GooberEatsAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PlacesController : ControllerBase
+    {
+        public string Index()
+        {
+            return "Controller works";
+        }
+    }
+}
--- GooberEats/GooberEatsAPI/Startup.cs
+++ GooberEats/GooberEatsAPI/Startup.cs
@@ -16,6 +16,7 @@ namespace GooberEatsAPI
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -30,10 +31,8 @@ namespace GooberEatsAPI
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                // Maps our default Controller route.
+                endpoints.MapControllerRoute("Default", "{controller=Places}/{action=Index}/{id?}");
             });
         }
     }
