1f25199ac273690de322ba6823220ff5104380bd
Robert James Nielsen <robert.j.nielsen@outlook.com>
2020-07-09 14:42:29 -0700

    Added Place model.

---
+++ GooberEats/GooberEatsAPI/Models/Place.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GooberEatsAPI.Models
+{
+    public class Place
+    {
+        public Place()
+        {
+
+        }
+
+        /// <summary>
+        /// The name of the Place.
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// The latitude of the Place.
+        /// </summary>
+        public double Latitude { get; set; }
+
+        /// <summary>
+        /// The longitude of the Place.
+        /// </summary>
+        public double Longitude { get; set; }
+
+        /// <summary>
+        /// The overall rating of the Place.
+        /// </summary>
+        public double Rating { get; set; }
+
+        /// <summary>
+        /// The total number of reviews for the Place.
+        /// </summary>
+        public int TotalReviews { get; set; }
+
+        /// <summary>
+        /// The address of the Place.
+        /// </summary>
+        public string Address { get; set; }
+    }
+}
